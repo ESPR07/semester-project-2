520e9db71e307a17159ed836bf9de2634dc693ae
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2023-11-22 13:02:56 +0100

    Images, Fonts, Navbar and some custom tailwind themes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**.html", "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      headerFont: ["Passion One", "Arial", "Times New Roman"],
+      mainFont: ["Ubuntu", "Arial", "Times New Roman"],
+    },
+    colors: {
+      navColor: "#363062",
+      importantElement: "#F99417",
+      whiteTone: "#F5F5F5",
+      lightBlue: "#2696B9",
+    },
+    extend: {
+      backgroundImage: {
+        bodyBackground: "url('../src/img/background-main.webp')",
+        avatarPlaceholder: "url('/src/img/placeholder.png')",
+        logo: "url('/src/img/eclipsebid-logo.webp')",
+        searchIcon: "url('/src/img/search-icon.svg')",
+      },
+    },
   },
   plugins: [],
 };
