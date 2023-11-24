2dbfb1adadd4fedf98afd93d0f360c81bd8ddbaf
ESPR07 <111272036+ESPR07@users.noreply.github.com>
2023-11-24 14:17:02 +0100

    coded static profile page an done some styling troubleshooting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,23 @@
 module.exports = {
   content: ["./**.html", "./src/**/*.{html,js}"],
   theme: {
-    fontFamily: {
-      headerFont: ["Passion One", "Arial", "Times New Roman"],
-      mainFont: ["Ubuntu", "Arial", "Times New Roman"],
-    },
-    colors: {
-      navColor: "#363062",
-      importantElement: "#F99417",
-      whiteTone: "#F5F5F5",
-      lightBlue: "#2696B9",
-      inputBackgroundDark: "#3a3b3c",
-    },
     extend: {
       backgroundImage: {
         bodyBackground: "url('../src/img/background-main.webp')",
         avatarPlaceholder: "url('/src/img/placeholder.png')",
         logo: "url('/src/img/eclipsebid-logo.webp')",
         searchIcon: "url('/src/img/search-icon.svg')",
+      },
+      fontFamily: {
+        headerFont: ["Passion One", "Arial", "Times New Roman"],
+        mainFont: ["Ubuntu", "Arial", "Times New Roman"],
+      },
+      colors: {
+        navColor: "#363062",
+        importantElement: "#F99417",
+        whiteTone: "#F5F5F5",
+        lightBlue: "#2696B9",
+        inputBackgroundDark: "#3a3b3c",
       },
     },
   },
